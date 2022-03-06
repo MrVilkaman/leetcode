20a7d586655f3cdd51994c0d116ed53daac9542f
zahar <zahar.zolotarev@gmail.com>
2022-03-06 20:12:32 +0300

    Подключил ktor и настроил server

--- busroutes/src/main/kotlin/ru/zolotarev/busroutes/Main.kt
+++ busroutes/src/main/kotlin/ru/zolotarev/busroutes/Main.kt
@@ -1,23 +1,59 @@
 package ru.zolotarev.busroutes
 
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 import ru.zolotarev.busroutes.models.DirectResponse
 import java.io.File
 
 
 class Main(
+    private val file: File = File("src/main/assets/routes.txt"),
     private val fileReader: PathFileReader = PathFileReaderImpl(),
     private val pathFinder: PathFinder = PathFinderImpl()
 ) {
 
-    fun main(from: Int, to: Int) {
+    fun main(from: Int, to: Int): String {
 
-        val toURI = File("src/main/assets/routes.txt").toURI()
-        val answer = fileReader.readFile(toURI) {
+        val answer = fileReader.readFile(file.toURI()) {
             val direct = pathFinder.existValidRoute(it, from, to)
 
             DirectResponse(from, to, direct)
         }
 
-        println(answer.toJson())
+        return answer.toJson()
     }
+}
+
+fun main() {
+    val main = Main(File("busroutes/src/main/assets/routes.txt"))
+    embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!")
+            }
+            get("/direct") {
+                val request = call.request
+                val from = request.queryParameters["from"]?.toInt()
+                val to = request.queryParameters["to"]?.toInt()
+
+                if (from == null || to == null) {
+
+                    call.respond(HttpStatusCode(400, "Ожидаются параметры from и to"))
+                    return@get
+                }
+
+                try {
+                    val response = main.main(from, to)
+                    call.respondText(response)
+                } catch (e: Exception) {
+                    call.respond(HttpStatusCode(500, e.toString()))
+                }
+
+            }
+        }
+    }.start(wait = true)
 }
